ac733b0174ca56bfe330aec3a0d0cfc215437581
lutobler <lutobler@student.ethz.ch>
2017-11-15 20:48:09 +0100

    Add stubs for Client, MessageSendTask classes

---
+++ Kompose/app/src/main/java/ch/ethz/inf/vs/kompose/kompose/MessageSendTask.java
@@ -0,0 +1,16 @@
+package ch.ethz.inf.vs.kompose.kompose;
+
+
+import android.os.AsyncTask;
+
+public class MessageSendTask extends AsyncTask<Void, Void, Void> {
+
+    // TODO
+    public MessageSendTask(Message message) {
+    }
+
+    // TODO
+    protected Void doInBackground(Void ... voids) {
+        return null;
+    }
+}
