3ddc7a4ac89b4be9455ba1134b1bbb348df41b9f
lutobler <lutobler@student.ethz.ch>
2017-11-15 13:34:05 +0100

    Move DownloadAudioTask to separate file

--- Kompose/app/src/main/java/ch/ethz/inf/vs/kompose/kompose/PlaylistItem.java
+++ Kompose/app/src/main/java/ch/ethz/inf/vs/kompose/kompose/PlaylistItem.java
@@ -3,18 +3,10 @@ package ch.ethz.inf.vs.kompose.kompose;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.util.Log;
 import android.util.SparseArray;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.lang.ref.WeakReference;
-import java.net.URL;
-import java.net.URLConnection;
 
 import at.huber.youtubeExtractor.VideoMeta;
 import at.huber.youtubeExtractor.YouTubeExtractor;
@@ -24,18 +16,56 @@ class PlaylistItem {
 
     private static final String LOG_TAG = "### PlaylistItem";
 
-    private boolean isDownloaded;
+    private boolean isDownloaded = false;
     private File storedFile;
-    private String youTubeUrl;
-    private  String downloadUrl;
-    private String title;
     private OnDownloadFinished onFinishedCallback;
     private Context context;
+
+    // original YouTube URL
+    private String youTubeUrl;
+
+    // extracted download URL
+    private String downloadUrl;
+
+    private String title;
+    private String id;
 
     PlaylistItem(Context context, String url) {
         isDownloaded = false;
         this.youTubeUrl = url;
         this.context = context;
+    }
+
+    boolean getIsDownloaded() {
+        return isDownloaded;
+    }
+
+    File getStoredFile() {
+        return storedFile;
+    }
+
+    OnDownloadFinished getOnFinishedCallback() {
+        return onFinishedCallback;
+    }
+
+    Context getContext() {
+        return context;
+    }
+
+    String getDownloadUrl() {
+        return downloadUrl;
+    }
+
+    String getTitle() {
+        return title;
+    }
+
+    void setStoredFile(File file) {
+        storedFile = file;
+    }
+
+    void setIsDownloaded(boolean status) {
+        isDownloaded = status;
     }
 
     /**
@@ -90,65 +120,5 @@ class PlaylistItem {
             }
         };
         youTubeExtractor.extract(youTubeUrl, true, false);
-    }
-
-    /**
-     * Start the download in an AsyncTask. When done, set isDownloaded to `true`,
-     * and call the onFinishedCallback, if it is registered.
-     *
-     * A WeakReference to this PlaylistItem is kept to allow the AsyncTask to be static and
-     * therefore avoid memory leaks.
-     * See: https://stackoverflow.com/questions/44309241/this-asynctask-class-should-be-static-or-leaks-might-occur
-     */
-    private static class DownloadAudioTask extends AsyncTask<String, Void, Void> {
-
-        private WeakReference<PlaylistItem> itemContext;
-
-        DownloadAudioTask(PlaylistItem itemContext) {
-            this.itemContext = new WeakReference<>(itemContext);
-        }
-
-        protected Void doInBackground(String ... urls) {
-            if (urls.length < 1) {
-                return null;
-            }
-
-            try {
-                PlaylistItem ctx = itemContext.get();
-                Log.d(LOG_TAG, "starting file download");
-                URL url = new URL(ctx.downloadUrl);
-                URLConnection connection = url.openConnection();
-                connection.connect();
-
-                // input stream
-                InputStream input = new BufferedInputStream(connection.getInputStream());
-
-                // output stream: file on internal storage
-                ctx.storedFile = new File(ctx.context.getCacheDir(), ctx.title + ".m4a");
-                OutputStream output = new FileOutputStream(ctx.storedFile);
-
-                // write data to file system
-                byte[] buffer = new byte[1024];
-                int count;
-                while ((count = input.read(buffer)) != -1) {
-                    output.write(buffer, 0, count);
-                }
-
-                Log.d(LOG_TAG, "stored file: " + ctx.storedFile.toString());
-                ctx.isDownloaded = true;
-                input.close();
-                output.close();
-
-                // issue the onFinished callback (if set)
-                if (ctx.onFinishedCallback != null) {
-                    ctx.onFinishedCallback.downloadFinished();
-                }
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return null;
-        }
     }
 }
