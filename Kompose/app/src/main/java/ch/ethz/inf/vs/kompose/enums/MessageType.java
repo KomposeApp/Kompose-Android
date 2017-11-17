1c1d0b7916e70dfe778260981f54aa1e8d43f4c4
lutobler <lutobler@student.ethz.ch>
2017-11-17 16:30:36 +0100

    Implement fromString in MessageType

--- Kompose/app/src/main/java/ch/ethz/inf/vs/kompose/enums/MessageType.java
+++ Kompose/app/src/main/java/ch/ethz/inf/vs/kompose/enums/MessageType.java
@@ -12,8 +12,13 @@ public enum MessageType {
     FINISH_SESSION,
     ERROR;
 
-    public MessageType fromString(String message) {
-        // todo
-        return MessageType.ERROR;
+    public MessageType fromString(String messageType) {
+        MessageType msg = MessageType.ERROR;
+        for (MessageType m : MessageType.values()) {
+            if (m.toString().equals(messageType)) {
+                msg = m;
+            }
+        }
+        return msg;
     }
 }
