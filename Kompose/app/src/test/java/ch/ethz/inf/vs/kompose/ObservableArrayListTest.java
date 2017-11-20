67f8097972c78bdff17d79fd167ab361fc736f40
lutobler <lutobler@student.ethz.ch>
2017-11-20 13:01:35 +0100

    Remove println statement

--- Kompose/app/src/test/java/ch/ethz/inf/vs/kompose/ObservableArrayListTest.java
+++ Kompose/app/src/test/java/ch/ethz/inf/vs/kompose/ObservableArrayListTest.java
@@ -39,7 +39,6 @@ public class ObservableArrayListTest {
 
         boolean sorted = true;
         for (int i = 0; i < list.size()-1; i++) {
-            System.out.println("" + list.get(i));
             if (list.get(i) > list.get(i+1)) {
                 sorted = false;
             }
