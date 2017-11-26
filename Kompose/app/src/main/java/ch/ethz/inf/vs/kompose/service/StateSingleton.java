d9d64511d0f91ad2f4cfa6251067c12ca77098b3
lutobler <lutobler@student.ethz.ch>
2017-11-26 12:46:52 +0100

    StateSingleton: Small cleanup

--- Kompose/app/src/main/java/ch/ethz/inf/vs/kompose/service/StateSingleton.java
+++ Kompose/app/src/main/java/ch/ethz/inf/vs/kompose/service/StateSingleton.java
@@ -1,9 +1,6 @@
 package ch.ethz.inf.vs.kompose.service;
 
-import android.content.Context;
 import android.databinding.ObservableList;
-import android.support.annotation.Nullable;
-import android.util.Log;
 
 import java.util.Comparator;
 import java.util.UUID;
@@ -14,8 +11,6 @@ import ch.ethz.inf.vs.kompose.model.list.ObservableUniqueSortedList;
 public class StateSingleton {
 
     private static final String LOG_TAG = "## SINGLETON HUB:";
-
-    public static final String CONNECTION_CHANGED_EVENT = "SessionService.CONNECTION_CHANGED_EVENT";
     private final String DIRECTORY_ARCHIVE = "session_archive";
 
     // Client specific fields
